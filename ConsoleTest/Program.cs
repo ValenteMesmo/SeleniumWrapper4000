6f0a30dcfe73dcf641b4b14600d19d4f5707c00d
Valente <ricardo.valente@impeto.com.br>
2019-05-23 14:40:32 -0300

    Uploading solution

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sut = new SeleniumWrapper4000.SeleniumWrapper(false))
+            {
+                sut.GoToUrl("http://google.com");
+            }
+
+        }
+    }
+}
