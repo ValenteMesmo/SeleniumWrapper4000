2899ab777da7a06a937a143cd5fe074473b50ef1
valentemesmo <valentemesmo@gmail.com>
2019-11-08 13:35:17 -0300

    Runing fsx to kill child process when test process is killed

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -4,14 +4,16 @@
     {
         static void Main(string[] args)
         {
-            using (var sut = new SeleniumWrapper4000.SeleniumWrapper(false,100000,SeleniumWrapper4000.BrowserType.InternetExplorer))
+            using (var sut = new SeleniumWrapper4000.SeleniumWrapper(
+                false
+                , 100000
+                , SeleniumWrapper4000.BrowserType.InternetExplorer))
             {
                 sut.GoToUrl("http://google.com");
 
                 int x = 5;
                 sut.Wait(() => x-- == 0);
             }
-
         }
     }
 }
---
+++ SeleniumWrapper4000/KillWithParent.fsx
@@ -0,0 +1,38 @@
+﻿open System.Diagnostics
+
+let args : string[] =
+    fsi.CommandLineArgs
+    |> Array.tail
+
+for i in 1..args.Length-1 do
+    System.Console.WriteLine args.[i]
+
+let getProcess id : (option<Process>) =
+    try
+        Some (Process.GetProcessById(id))
+    with ex ->
+        None
+
+if (args.Length < 2) then
+    System.Console.WriteLine "ParentId and at least one childId are required!"
+else
+    let parentProcess = int args.[0]
+
+    let mutable parentIsRunning = true
+    while parentIsRunning do
+        System.Threading.Thread.Sleep(100)
+        let parentProcess = getProcess parentProcess
+
+        if parentProcess.IsNone then
+            parentIsRunning <- false
+
+    for i in 1..args.Length-1 do
+        let child = getProcess <| int args.[i]
+        if child.IsSome then
+            try
+                child.Value.Kill()
+                ()
+            with ex ->
+                ()
+
+0
