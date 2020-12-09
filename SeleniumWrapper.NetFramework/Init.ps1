a11853c212ef5caa31c71e1a30c603b177d5e218
valentemesmo <valentemesmo@gmail.com>
2020-12-09 20:52:06 -0300

    trying to use Init.ps1

---
+++ SeleniumWrapper.NetFramework/Init.ps1
@@ -0,0 +1,11 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$file1 = $project.ProjectItems.Item("chromedriver.exe")
+$file2 = $project.ProjectItems.Item("KillWithParent.fsx")
+
+# set 'Copy To Output Directory' to 'Copy if newer'
+$copyToOutput1 = $file1.Properties.Item("CopyToOutputDirectory")
+$copyToOutput1.Value = 1
+
+$copyToOutput2 = $file2.Properties.Item("CopyToOutputDirectory")
+$copyToOutput2.Value = 2
